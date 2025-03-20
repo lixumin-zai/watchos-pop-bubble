3edc7d4ebc07ce57d1ab8130466a1b99703a0a03
lismin <lixumin1030@gmail.com>
2025-03-20 15:48:35 +0800

    Initial Commit

---
+++ BubbleJoy Watch App/BubbleJoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleJoyApp.swift
+//  BubbleJoy Watch App
+//
+//  Created by lixumin on 2025/3/20.
+//
+
+import SwiftUI
+
+@main
+struct BubbleJoy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
